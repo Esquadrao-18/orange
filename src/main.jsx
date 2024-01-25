bfba4940224a60e544146852977fd27e933f6fc7
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-01-25 16:38:59 -0300

    Style: Inclui padrão de cor e fontes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
+import { ThemeProvider, createTheme } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
-
+const secondary = {
+  main: '#FF5522',
+  dark: '#CC4400',
+  light: '#FFEECC'
+}
+const theme = createTheme({
+  palette: {
+    secondary: secondary
+  }
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
