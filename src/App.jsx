df708253cacf6ab5b0863b84077ae7d00de64c6a
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-01-26 21:36:34 -0300

    fix: Reorganiza criação do tema principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import Profile from './pages/Profile/Profile'
-
-function App() {
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './routes/routes'
+const secondary = {
+  main: '#FF5522',
+  dark: '#CC4400',
+  light: '#FFEECC'
+}
+const principal = {
+  main: '#EDEFF2'
+}
+const theme = createTheme({
+  palette: {
+    secondary: secondary,
+    principal: principal
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: 500,
+          fontSize: '15px',
+          padding: '8px 22px',
+          lineHeight: '26px'
+        }
+      }
+    }
+  }
+})
+export default function App() {
   return (
-    <>
-      <Profile></Profile>
-    </>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div className="App">
+          <Router />
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
-import { ThemeProvider, createTheme } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
-const secondary = {
-  main: '#FF5522',
-  dark: '#CC4400',
-  light: '#FFEECC'
-}
-const theme = createTheme({
-  palette: {
-    secondary: secondary
-  }
-})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 )
