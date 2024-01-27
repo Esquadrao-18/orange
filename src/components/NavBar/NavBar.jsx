4bd637be6fb02bd595f293e7d7cc5bf9ae9d393e
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-01-26 21:35:36 -0300

    feat: Inclui menu para versão mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,89 @@
+import LogoutIcon from '@mui/icons-material/Logout'
+import MenuIcon from '@mui/icons-material/Menu'
+import {
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Typography
+} from '@mui/material'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import orangeLogo from '../../assets/logo-orange.svg'
-import iconMenuMobile from '../../assets/menu-mobile-icon.svg'
 import notificationsIcon from '../../assets/notifications-icon.svg'
 import profileImg from '../../assets/profile-image.svg'
 import './style.css'
+
 function NavBar() {
+  const [anchorElUser, setAnchorElUser] = useState(null)
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget)
+  }
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
   return (
     <nav className="w-full flex py-4 px-[30px] items-center justify-between nav-element">
-      <section className="flex section-logo-menu">
-        <img
-          className="sm:hidden p-2"
-          src={iconMenuMobile}
-          alt="Ícone com três linhas brancas representando menu"
-        />
-        <img src={orangeLogo} alt="Logotipo Orange Juice Portfolio" />
+      <section className="flex section-logo-menu items-center">
+        <IconButton
+          size="large"
+          edge="start"
+          color="text.secondary"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={handleOpenUserMenu}
+        >
+          <MenuIcon color="principal" fontSize="large" />
+        </IconButton>
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem onClick={handleCloseUserMenu}>
+            <Typography>Camila</Typography>
+          </MenuItem>
+          <MenuItem onClick={handleCloseUserMenu}>
+            <Typography>camila.ux@gmail.com</Typography>
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={handleCloseUserMenu}>
+            <Typography>Projetos</Typography>
+          </MenuItem>
+          <MenuItem onClick={handleCloseUserMenu}>
+            <Typography>Descobrir</Typography>
+          </MenuItem>
+          <Divider />
+          <MenuItem onClick={handleCloseUserMenu}>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <Typography>Sair</Typography>
+          </MenuItem>
+        </Menu>
+        <Link to="/">
+          <img src={orangeLogo} alt="Logotipo Orange Juice Portfolio" />
+        </Link>
+
         <ul className="list-none sm:flex hidden items-center flex-1 gap-6">
           <li>
-            <a>Meus projetos</a>
+            <Link to="/meus-projetos">Meus Projetos</Link>
           </li>
           <li>
-            <a>Descobrir</a>
+            <Link to="/descobrir">Descobrir</Link>
           </li>
         </ul>
       </section>
