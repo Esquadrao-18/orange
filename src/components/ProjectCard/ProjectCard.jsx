7bdd06cb4b53da788277473558ce98c27ca5e04f
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-01-29 01:50:49 -0300

    refactor: Ajusta componente de card de projeto e listagem para ser reutilizado

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@emotion/react'
 import { Edit } from '@mui/icons-material'
 import {
   Card,
@@ -6,14 +7,35 @@ import {
   IconButton,
   Menu,
   MenuItem,
-  Typography
+  Typography,
+  useMediaQuery
 } from '@mui/material'
 import { useState } from 'react'
 import profileImg from '../../assets/profile-image.svg'
 
 export default function ProjectCard(props) {
-  const { img, tags, date, first } = props
-  console.log(first)
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
+  const infosUserStyle = {
+    width: '105px',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  const cardUser = () => {
+    return (
+      <>
+        <Typography sx={{ color: isSmallScreen ? '#303133' : '#515255' }}>
+          Camila Soares
+        </Typography>
+        {isSmallScreen ? undefined : (
+          <Typography sx={{ color: '#515255' }}>&nbsp;•&nbsp;</Typography>
+        )}
+        <Typography sx={{ color: '#515255' }}>{date}</Typography>
+      </>
+    )
+  }
+
+  const { img, tags, date, isPersonal } = props
   const [element, setElement] = useState(null)
 
   const handleOpenSubMenu = (event) => {
@@ -24,97 +46,110 @@ export default function ProjectCard(props) {
   }
   return (
     <Card sx={{ maxWidth: 389, position: 'relative', boxShadow: 'none' }}>
-      <IconButton
-        sx={{ position: 'absolute', right: 0 }}
-        onClick={handleOpenSubMenu}
-      >
-        <Edit
-          sx={{
-            color: '#303133',
-            backgroundColor: '#FFCC99',
-            borderRadius: '50%',
-            fontSize: '28px',
-            padding: '0.8px'
-          }}
-        />
-      </IconButton>
-      <Menu
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiMenu-list': {
-              minWidth: 208,
-              minHeight: 104,
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
+      {isPersonal ? (
+        <>
+          {' '}
+          <IconButton
+            sx={{ position: 'absolute', right: 0 }}
+            onClick={handleOpenSubMenu}
+          >
+            <Edit
+              sx={{
+                color: '#303133',
+                backgroundColor: '#FFCC99',
+                borderRadius: '50%',
+                fontSize: '28px',
+                padding: '0.8px'
+              }}
+            />
+          </IconButton>
+          <Menu
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                boxShadow: '2px 2px 8px 0px rgba(0, 0, 0, 0.30)',
+                mt: 1.5,
+                '& .MuiMenu-list': {
+                  minWidth: 208,
+                  minHeight: 104,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  gap: '8px',
+                  padding: '16px 0px'
+                },
+                '&::before': {
+                  content: '""',
+                  display: 'block',
+                  position: 'absolute',
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: 'background.paper',
+                  transform: 'translateY(-50%) rotate(45deg)',
+                  zIndex: 0
+                }
+              }
+            }}
+            color="secondary"
+            sx={{
+              mt: '28px',
+              ml: '-16px',
               gap: '8px',
-              padding: '16px 0px'
-            },
-            '&::before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0
-            }
-          }
-        }}
-        color="secondary"
-        sx={{
-          mt: '28px',
-          ml: '-16px',
-          gap: '8px',
-          padding: '90px'
-        }}
-        id="menu-appbar"
-        anchorEl={element}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right'
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right'
-        }}
-        open={Boolean(element)}
-        onClose={handleCloseSubMenu}
-      >
-        <MenuItem
-          sx={{ ':hover': { backgroundColor: '#FEC' } }}
-          onClick={handleCloseSubMenu}
-        >
-          <Typography>Editar</Typography>
-        </MenuItem>
-        <MenuItem
-          sx={{ ':hover': { backgroundColor: '#FEC' } }}
-          onClick={handleCloseSubMenu}
-        >
-          <Typography>Excluir</Typography>
-        </MenuItem>
-      </Menu>
+              padding: '90px'
+            }}
+            id="menu-appbar"
+            anchorEl={element}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right'
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right'
+            }}
+            open={Boolean(element)}
+            onClose={handleCloseSubMenu}
+          >
+            <MenuItem
+              sx={{ ':hover': { backgroundColor: '#FEC' } }}
+              onClick={handleCloseSubMenu}
+            >
+              <Typography>Editar</Typography>
+            </MenuItem>
+            <MenuItem
+              sx={{ ':hover': { backgroundColor: '#FEC' } }}
+              onClick={handleCloseSubMenu}
+            >
+              <Typography>Excluir</Typography>
+            </MenuItem>
+          </Menu>{' '}
+        </>
+      ) : undefined}
+
       <CardMedia component="img" width="389" height="258" image={img} alt="" />
       <section className="flex justify-between pt-2">
         <div className="flex items-center gap-2">
           <img src={profileImg} className="w-6 rounded-full" />
-          <Typography sx={{ color: '#515255' }}>
-            Camila Soares • {date}
-          </Typography>
+
+          <div
+            className="flex"
+            style={isSmallScreen ? infosUserStyle : undefined}
+          >
+            {cardUser()}
+          </div>
         </div>
-        <div className="flex gap-2">
-          {tags.map((tag, index) => (
-            <Chip key={index} label={tag} sx={{ fontSize: 13 }} />
-          ))}
-        </div>
+        {isPersonal ? (
+          <div className="flex gap-2">
+            {tags.map((tag, index) => (
+              <Chip key={index} label={tag} sx={{ fontSize: 13 }} />
+            ))}
+          </div>
+        ) : undefined}
       </section>
     </Card>
   )
--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,16 +1,25 @@
 import { Skeleton, TextField } from '@mui/material'
 import AddProjectCard from '../AddProjectCard/AddProjectCard'
 import ProjectCard from '../ProjectCard/ProjectCard'
+import './style.css'
 
 export default function ProjectsList(props) {
-  const { projects, isLoading } = props
+  const { projects, isLoading, isPersonal } = props
   return (
-    <section className="w-screen flex flex-col px-20 projects-area">
-      <div className="projects-area-top w-[43%]">
-        <h4 className="text-xl font-medium opacity-60 mb-2">Meus projetos</h4>
+    <section className="w-screen flex flex-col px-8">
+      <div className="projects-area-top  sm:w-[43%]  w-full">
+        {isPersonal ? (
+          <h4
+            className="text-xl font-medium opacity-60 mb-2"
+            style={{ color: '#0b0c0d' }}
+          >
+            Meus projetos
+          </h4>
+        ) : undefined}
+
         <TextField fullWidth label="Buscar tags" variant="outlined" />
       </div>
-      <div className="projects-list flex flex-wrap gap-6 my-10">
+      <div className="flex flex-wrap gap-6 my-10">
         {isLoading ? (
           <>
             <Skeleton variant="rectangular" width={389} height={258} />
@@ -21,6 +30,7 @@ export default function ProjectsList(props) {
         ) : projects.length > 0 ? (
           projects.map((project) => (
             <ProjectCard
+              isPersonal={isPersonal}
               key={project.id}
               img={project.img}
               tags={project.tags}
