23914219cfa5327c01214ca5fc675e659f21bdae
Camila Braz <camilabvieira74@gmail.com>
2024-01-29 20:23:55 -0300

    chore: Adiciona validação do React Hook Form ao formulário existente

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,29 @@
 import Banner from "../../components/Banner/banner"
 import loginBanner from "../../assets/login-banner.svg"
 import { TextField , Button } from "@mui/material"
-import iconGoogle from "../../assets/icon-google.svg"   
+import iconGoogle from "../../assets/icon-google.svg"
+import { useForm, Controller } from "react-hook-form"   
 
 function Login () {
+
+        const { control, handleSubmit, formState } = useForm();
+        const { errors } = formState;
+    
+        const onSubmit = (data) => {
+        console.log(data);
+        }
+
     return (
         <main className="flex">
             <section>
                 <Banner src={loginBanner} />
             </section>
             
-            <section className="flex flex-col justify-center 
-            items-center h-screen mx-auto font-sans: Roboto">
+            <section className="flex flex-col justify-center items-center h-screen mx-auto font-sans: Roboto">
                 
-                <h1 className="text-2xl sm:text-5xl" style={{ color: '#222244'}}>
+                <h3 className="text-2xl sm:text-5xl" style={{ color: '#222244'}}>
                     Entre no Orange Portfólio
-                </h1>
+                </h3>
 
                 <button className="btn btn-secondary my-8 flex text-sm p-3 items-start Neutral 
                  bg-white rounded-md shadow-md">
@@ -23,23 +31,45 @@ function Login () {
                     <span className="font-bold" style={{color: '#0000008A'}}> Entrar com o Google</span>
                 </button>
                 
-                <form className="flex flex-col gap-4 w-80 sm:w-full" >
+                <form onSubmit={handleSubmit(onSubmit)} noValidate className="flex flex-col gap-4 w-80 sm:w-full" >
 
-                    <h2 className="lg:text-2xl pb-2" style={{ color: '#515255'}}>
+                    <h5 className="lg:text-2xl pb-2" style={{ color: '#515255'}}>
                         Faça login com email
-                    </h2>
+                    </h5>
                     
-                    <TextField
-                    label="Email address"
-                    variant="outlined"
-                    size="large"/>
+                    <Controller 
+                        name="email"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => (
+                        
+                        <TextField
+                            label="Email address"
+                            variant="outlined"
+                            size="large"
+                            {...field}
+                            error={!!errors.email}
+                        />
+                        )}
+                    />
 
-                    <TextField id="outlined-password-input" 
-                    label="Password" 
-                    type="password" 
-                    autoComplete="current-password"/>
+                     <Controller
+                        name="password"
+                        control={control}
+                        rules={{ required: true }}
+                        render={({ field }) => (
+                            <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            {...field}
+                            error={!!errors.password}
+                        />
+                    )}
+                />
 
-                    <Button variant="contained"
+                    <Button type="submit" variant="contained"
                     color="secondary"
                     size="medium">
                     Entrar
