7c9d818da4bebb5a0dc0264c2e9a961ce53ea8c7
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-01-30 17:06:08 -0300

    feat: Implementa entrada de imagem e chamada da modal

--- src/components/AddProjectCard/AddProjectCard.jsx
+++ src/components/AddProjectCard/AddProjectCard.jsx
@@ -1,38 +1,60 @@
 import { Collections } from '@mui/icons-material'
 import { CardContent, IconButton, Typography } from '@mui/material'
+import { useState } from 'react'
+import NewProject from '../../modals/NewProjectModal/NewProjectModal'
 
-export default function AddProjectCard() {
+export default function AddProjectCard(props) {
+  const { isModal } = props
+  const [modalVisible, setModalVisible] = useState(false)
+  function handleOpenModalAddProject() {
+    setModalVisible(true)
+  }
   return (
-    <CardContent
-      sx={{
-        padding: 4,
-        backgroundColor: '#E6E9F2',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 389,
-        height: 258,
-        gap: '16px'
-      }}
-    >
-      <IconButton
-        disableRipple={true}
+    <>
+      <NewProject visible={modalVisible} onClose={setModalVisible} />
+
+      <CardContent
         sx={{
-          ':hover': { backgroundColor: 'transparent' }
+          padding: 4,
+          backgroundColor: '#E6E9F2',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minWidth: 312,
+          maxWidth: 389,
+          height: 258,
+          gap: '16px'
         }}
       >
-        <Collections sx={{ fontSize: '46px', color: '#323232' }} />
-      </IconButton>
-      <Typography sx={{ fontSize: 16, opacity: 0.6 }} gutterBottom>
-        Adicione seu primeiro projeto
-      </Typography>
-      <Typography
-        sx={{ fontSize: 14, opacity: 0.6, width: 270 }}
-        component="div"
-      >
-        Compartilhe seu talento com milhares de pessoas
-      </Typography>
-    </CardContent>
+        <input
+          accept="image/*"
+          style={{ display: 'none' }}
+          multiple
+          type="file"
+        />
+        <IconButton
+          onClick={() => handleOpenModalAddProject()}
+          disableRipple={true}
+          sx={{
+            ':hover': { backgroundColor: 'transparent' }
+          }}
+        >
+          <Collections sx={{ fontSize: '46px', color: '#323232' }} />
+        </IconButton>
+        {isModal ? undefined : (
+          <Typography sx={{ fontSize: 16, opacity: 0.6 }} gutterBottom>
+            Adicione seu primeiro projeto
+          </Typography>
+        )}
+
+        <Typography
+          sx={{ fontSize: 14, opacity: 0.6, width: 270 }}
+          component="div"
+        >
+          Compartilhe seu talento com milhares de pessoas
+        </Typography>
+      </CardContent>
+    </>
   )
 }
