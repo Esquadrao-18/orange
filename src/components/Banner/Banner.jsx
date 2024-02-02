d648060379be9bf75ad15e6e928b9360859e7704
Camila Braz <camilabvieira74@gmail.com>
2024-02-02 10:57:14 -0300

    style(banner): otimiza responsividade para diferentes tamanhos de tela

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 function Banner (props) {
     return (
       
-      <div className="image hidden sm:block h-screen">
+      <div className="image hidden lg:md:sm:block h-screen">
         <img className="object-contain h-full" src={props.src} alt="login-banner" />
       </div>
     )
